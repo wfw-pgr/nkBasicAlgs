169ea8d8b77c444a7c66182e5305c0c7cbf3f9f1
wfw-pgr <wfw.pgr@gmail.com>
2021-08-24 11:51:53 +0900

    modify sort__structured to adapt to the 2D case.

--- sort__structuredData.py
+++ sort__structuredData.py
@@ -33,12 +33,19 @@ def sort__structuredData( Data=None, order="ijk", dim=3, component_index=None ):
     yMin,yMax = np.min( Data_[:,y_] ), np.max( Data_[:,y_] )
     zMin,zMax = np.min( Data_[:,z_] ), np.max( Data_[:,z_] )
 
+    if ( zMax - zMin == 0.0 ):
+        dim = 2
+    
     # ------------------------------------------------- #
     # --- [3] index making                          --- #
     # ------------------------------------------------- #
-    index_x   = ( ( np.ravel( Data_[:,x_] ) - xMin ) / ( xMax - xMin ) * LI )
-    index_y   = ( ( np.ravel( Data_[:,y_] ) - yMin ) / ( yMax - yMin ) * LJ )
-    index_z   = ( ( np.ravel( Data_[:,z_] ) - zMin ) / ( zMax - zMin ) * LK )
+    if   ( dim == 2 ):
+        index_x   = ( ( np.ravel( Data_[:,x_] ) - xMin ) / ( xMax - xMin ) * LI )
+        index_y   = ( ( np.ravel( Data_[:,y_] ) - yMin ) / ( yMax - yMin ) * LJ )
+    elif ( dim == 3 ):
+        index_x   = ( ( np.ravel( Data_[:,x_] ) - xMin ) / ( xMax - xMin ) * LI )
+        index_y   = ( ( np.ravel( Data_[:,y_] ) - yMin ) / ( yMax - yMin ) * LJ )
+        index_z   = ( ( np.ravel( Data_[:,z_] ) - zMin ) / ( zMax - zMin ) * LK )
     idigit   = int( "1" + len( str( LI ) ) * "0" )
     jdigit   = int( "1" + len( str( LJ ) ) * "0" )
     kdigit   = int( "1" + len( str( LK ) ) * "0" )
@@ -46,12 +53,20 @@ def sort__structuredData( Data=None, order="ijk", dim=3, component_index=None ):
     # ------------------------------------------------- #
     # --- [4] sorting                               --- #
     # ------------------------------------------------- #
-    if   ( order.lower() == "ijk" ):
-        index = index_x + index_y*idigit + index_z*idigit*jdigit
-        shape = (LK,LJ,LI,nCmp)
-    elif ( order.lower() == "kji" ):
-        index = index_z + index_y*kdigit + index_x*jdigit*kdigit
-        shape = (LI,LJ,LK,nCmp)
+    if ( dim == 2 ):
+        if   ( order.lower() == "ijk" ):
+            index = index_x + index_y*idigit
+            shape = (LJ,LI,nCmp)
+        elif ( order.lower() == "kji" ):
+            index = index_z + index_y*kdigit
+            shape = (LI,LJ,nCmp)
+    if ( dim == 3 ):
+        if   ( order.lower() == "ijk" ):
+            index = index_x + index_y*idigit + index_z*idigit*jdigit
+            shape = (LK,LJ,LI,nCmp)
+        elif ( order.lower() == "kji" ):
+            index = index_z + index_y*kdigit + index_x*jdigit*kdigit
+            shape = (LI,LJ,LK,nCmp)
     sort      = np.argsort( index )
     Data_     = Data_[sort,:]
     Data_     = np.reshape( Data_, shape )
@@ -67,7 +82,7 @@ if ( __name__=="__main__" ):
     import nkUtilities.equiSpaceGrid as esg
     x1MinMaxNum = [ 0.0, 1.0, 11 ]
     x2MinMaxNum = [ 0.0, 1.0, 11 ]
-    x3MinMaxNum = [ 0.0, 1.0, 11 ]
+    x3MinMaxNum = [ 0.0, 0.0,  1 ]
     coord       = esg.equiSpaceGrid( x1MinMaxNum=x1MinMaxNum, x2MinMaxNum=x2MinMaxNum, \
                                      x3MinMaxNum=x3MinMaxNum, returnType = "point" )
     field       = np.copy( coord )
